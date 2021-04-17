ba416781180ca91a610447a134431d719c6ef048
arya <pram.aryasakamki225@protonmail.ch>
2021-04-18 06:05:49 +0700

    🌐 Fix some bugs and typo

--- lib/routes/itemRoute.ts
+++ lib/routes/itemRoute.ts
@@ -3,9 +3,11 @@ import {ItemController} from "../controllers/ItemController"
 
 
 export class Routes {
+public itemController: ItemController = new ItemController()
 	public routes(app): void {
-	const itemController: ItemController = new ItemController()
 		app.route('/')
+		.get(this.itemController.itemGet)
+		.post(this.itemController.addItem)
 		app.route('/:id')
 		.get((req: Request,res: Response) => {
 			res.status(200).send({
